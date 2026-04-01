13319837336c7a562773b28a2ea72f73f4ab4871
Irwin Razaghi <irwin@razaghi.com.au>
2026-04-01 12:39:11 +1100

    fix: rename frame file to bust stubborn browser cache

--- www/samsung-epaper-card.js
+++ www/samsung-epaper-card.js
@@ -533,7 +533,7 @@ class SamsungEpaperCard extends HTMLElement {
         }
         .frame-overlay {
           position:absolute; top:0; left:0; width:100%; height:100%;
-          background:url('/local/frame.png?v=7') center/contain no-repeat;
+          background:url('/local/epaper-frame.png') center/contain no-repeat;
           z-index:2; pointer-events:none;
         }
         .frame-inner {
